db20c579b24fb91e703435e23bff3746df6593a5
alubowsky <zevelmail13@gmail.com>
2018-03-22 15:40:09 -0500

    complete

--- map app (JS)/maps/map.js
+++ map app (JS)/maps/map.js
@@ -1,44 +1,65 @@
 /*global google, $*/
 /* jshint -W104*/
 
-(function(){
+(function () {
   "use strict";
 
   //set default location
-  let location = {lat: 41.8781, lng: -87.6298},
+  let location = { lat: 41.8781, lng: -87.6298 },
 
-  //bounds var to set how big map is
-  bounds,
+    //bounds var to set how big map is
+    bounds,
 
-  places = [],
+    places = [],
 
-  //the search box bounds
-  searchBounds = new google.maps.LatLngBounds(
-    new google.maps.LatLng(85, -180),           // top left corner of map
-    new google.maps.LatLng(-85, 180)            // bottom right corner
-  ),
+    //grab hold of the list that the places will be placed in
+    theList = $('#picDiv ul'),
 
-  //search box val
-  search = $('#search'),
+    //the search box bounds
+    searchBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(85, -180),           // top left corner of map
+      new google.maps.LatLng(-85, 180)            // bottom right corner
+    ),
 
-  //create map
-  map = new google.maps.Map(
-    document.getElementById('map'),
-    {
+    //search box val
+    search = $('#search'),
+
+    //if in search mode
+    setSearch = false,
+
+    //create map
+    map = new google.maps.Map(
+      document.getElementById('map'),
+      {
         center: location,
         zoom: 2
+      }
+    ),
+
+    //create info window
+    infoWindow = new google.maps.InfoWindow({
+      maxWidth: 250
+    }),
+
+    //array of the markers on the map
+    markers = [],
+
+    //service to do the searches
+    service = new google.maps.places.PlacesService(map);
+
+
+  //function to clear old markers
+  function clearmap() {
+    /*if(rectangle !== null){
+        rectangle.setMap(null);
+    }*/
+    if (markers) {
+      markers.forEach(function (marker) {
+        marker.setMap(null);
+      });
     }
-  ),
-  //create info window
-  infoWindow = new google.maps.InfoWindow({
-    maxWidth: 250
-  }),
-
-  //array of the markers on the map
-  markers = [],
-
-  //service to do the searches
-  service = new google.maps.places.PlacesService(map);
+    theList.empty();
+  }
 
   //function to handle geolocation error
   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -50,7 +71,7 @@
   }
 
   //function search for synagogue nearby in 1000 radius
-  function findSynagoguesNearby (nearby){
+  function findSynagoguesNearby(nearby) {
     let request = {
       location: nearby,
       radius: '5000',
@@ -68,7 +89,9 @@
   }
 
   //function search for synagogue search box in 1000 radius
-  function findSynagoguesSearch (search){
+  function findSynagoguesSearch(search) {
+    setSearch = true;
+    clearmap();
     let request = {
       query: search,
       type: ['synagogue']
@@ -78,26 +101,39 @@
 
   //pagination for more results
   var getNextPage = null;
-  var moreButton = document.getElementById('more');
-    moreButton.onclick = function() {
-    moreButton.disabled = true;
+  var moreButton = $('#more');
+  moreButton.click(function () {
+    moreButton.attr('disabled', 'disabled');
     if (getNextPage) getNextPage();
-  };
+  });
 
   //callback for the search - places marker at found locations
   function searchCallback(results, status, pagination) {
+    bounds = new google.maps.LatLngBounds();
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-      console.log(results);
       for (var i = 0; i < results.length; i++) {
-      let place = results[i],
-      url = place.photos ? place.photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200}) : "default.png";
-      $('<li><img src= "' + url + '"/><div id = "details"><h5>' + place.name + '</h5> <p>' + place.vicinity + '</p></div></li>').appendTo($('#picDiv ul'));
-      places.push(place);
+        let place = results[i],
+          url = place.photos ? place.photos[0].getUrl({ 'maxWidth': 200, 'maxHeight': 200 }) : "default.png",
+          address = setSearch ? place.formatted_address : place.vicinity;
+        bounds.extend(place.geometry.location);
+        map.fitBounds(bounds);
+        console.log(place);
+        $('<li><img src= "' + url + '"/><div id = "details">' +
+          '<h5>' + place.name + '</h5>' +
+          '<p>' + address + '</p></div></li>')
+          .appendTo(theList)
+          .click(() => {
+            map.panTo(place.geometry.location);
+            map.setZoom(15);
+          });
+        places.push(place);
         createMarker(place.geometry.location, place.name);
       }
 
       //pagination for more results
-      getNextPage = pagination.hasNextPage && function() {
+      moreButton.show();
+      moreButton.attr('disabled', !pagination.hasNextPage);
+      getNextPage = pagination.hasNextPage && function () {
         pagination.nextPage();
       };
     }
@@ -105,31 +141,28 @@
 
   //creates a marker on the map
   function createMarker(place, name) {
-      var marker = new google.maps.Marker({
-        map: map,
-        position: place,
-        title: name
-      });
-      markers.push(marker);
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place,
+      title: name
+    });
+    markers.push(marker);
   }
 
   //geolocation
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
       location = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-    map.setCenter(location);
-    map.setZoom(13);
-    bounds =  map.getBounds();
-    findSynagoguesNearby(location);
-    map.fitBounds(bounds);
-    $('#more').show();
+      map.setCenter(location);
+      findSynagoguesNearby(location);
+      //$('#more').show();
     },
-    function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
+      function () {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
   } else {
     //if user doesn't allow
     handleLocationError(false, infoWindow, map.getCenter());
@@ -138,8 +171,8 @@
   //using search box
   $('#go').click(function () {
     findSynagoguesSearch(search.val());
-    $('#more').show();
+    //$('#more').show();
   });
-   
+
 
 }());
